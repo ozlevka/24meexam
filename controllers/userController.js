27b221a3377f7105f5beca91a6a110d7985edbaa
levka <ozlevka@levka-pythondev.(none)>
2013-05-14 14:37:21 +0300

    add static content to SC

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,11 @@ var userDb = couchdb.db('exam_db');
 
 module.exports.controller = function(req,res,next)
 {
-		
+	if (req.method == 'GET' && req.url == '/') 
+	{
+		res.render('index',{});
+		return;
+	}
 	next();
 }; 
 
